e4caed1f93ffe3ba958c622946aae3dda4502649
Mario <luix30lp@gmail.com>
2019-09-29 20:36:38 -0500

    login and signin views

--- app/src/main/java/com/sistemas/itqin/MainActivity.java
+++ app/src/main/java/com/sistemas/itqin/MainActivity.java
@@ -1,14 +1,45 @@
 package com.sistemas.itqin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_person_black_24dp,
+            R.drawable.ic_person_add_black_24dp
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LoginFragment(), "Ingresar");
+        adapter.addFragment(new SignInFragment(), "Crear Cuenta");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
     }
+
+
 }
